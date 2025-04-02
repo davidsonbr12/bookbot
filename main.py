96700013897465bf61cfcb20c6c35d07bac70ee3
davidsonbr12 <davidsonbr12@gmail.com>
2025-04-01 19:59:38 -0700

    output the number of words in the file to console

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
 
 def get_book_text(path):
@@ -9,6 +10,10 @@ def get_book_text(path):
         return f.read()
 
 
-main()
+def get_num_words(text):
+    words = text.split()
+    num_words = len(words)
+    return num_words
 
+main()
 
